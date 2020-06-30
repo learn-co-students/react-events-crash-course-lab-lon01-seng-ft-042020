055a4fe91596cb45dff56c87b6e4aa5112029f91
github username <github email address>
2020-06-30 02:50:18 +0100

    Done.

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -28,7 +28,6 @@ export default class ChromeBoisDomain extends Component {
   /* if the key pressed was 'a', then it should call `resize` with '+'
   /* if the key pressed was 's', then it should call `resize` with '-' 
    */
-
   handleKeyDown = (event) => {
   // debugger
   if (event.key == "a") {return resize("+")}
